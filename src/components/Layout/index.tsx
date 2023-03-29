17ac504420bd36ea563abd031d0e9d57de672abd
denisprado <denisforigo@gmail.com>
2023-03-29 13:00:42 -0300

    fix tsc errors 4

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,8 @@
 import Footer from "@components/Footer";
-import Header from "@components/Header";
 import { ReactNode } from "react";
 import Contact from "@components/Contact";
 import Box from "@components/Box";
+import ClientHeader from "@components/Header";
 
 interface LayoutProps {
   children: ReactNode;
@@ -11,7 +11,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col items-center justify-center w-full">
-      <Header />
+      <ClientHeader />
       {children}
       <Box className="w-full px-4" color="neutral">
         <Contact />

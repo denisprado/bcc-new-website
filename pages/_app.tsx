edf5e05697484dfa2785510f190f7e6874233274
denisprado <denisforigo@gmail.com>
2023-03-29 10:35:06 -0300

    fix tsc errors 3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import { Layout as RefineLayout, notificationProvider } from "@refinedev/antd";
-import { GitHubBanner, Refine } from "@refinedev/core";
+import { Refine } from "@refinedev/core";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import routerProvider, {
-	UnsavedChangesNotifier,
+  UnsavedChangesNotifier,
 } from "@refinedev/nextjs-router";
 import type { NextPage } from "next";
 import { AppProps } from "next/app";
@@ -14,85 +14,85 @@ import { dataProvider } from "@refinedev/supabase";
 import { authProvider } from "src/authProvider";
 import { supabaseClient } from "src/utility";
 import { ReactElement, ReactNode, useState } from "react";
-import '../src/styles/styles.css';
-import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
+import "../src/styles/styles.css";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-	noLayout?: boolean;
-	getLayout?: (page: ReactElement) => ReactNode;
+export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
+  noLayout?: boolean;
+  getLayout?: (page: ReactElement) => ReactNode;
 };
 
 type AppPropsWithLayout = AppProps & {
-	Component: NextPageWithLayout;
+  Component: NextPageWithLayout;
 };
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
-	const [queryClient] = useState(() => new QueryClient())
-	const renderComponent = () => {
-		const getLayout = Component.getLayout ?? ((page) => page)
-		if (Component.noLayout) {
-			return (
-				<QueryClientProvider client={queryClient}>
-					<Hydrate state={pageProps.dehydratedState}>
-						{getLayout(<Component {...pageProps} />)}
-					</Hydrate>
-					<ReactQueryDevtools />
-				</QueryClientProvider>
-			);
-		}
+  const [queryClient] = useState(() => new QueryClient());
+  const renderComponent = () => {
+    const getLayout = Component.getLayout ?? ((page) => page);
+    if (Component.noLayout) {
+      return (
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            {getLayout(<Component {...pageProps} />)}
+          </Hydrate>
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      );
+    }
 
-		return (
-			<RefineLayout Header={Header}>
-				<Component {...pageProps} />
-			</RefineLayout>
-		);
-	};
+    return (
+      <RefineLayout Header={Header}>
+        <Component {...pageProps} />
+      </RefineLayout>
+    );
+  };
 
-	return (
-		<>
-			<RefineKbarProvider>
-				<ColorModeContextProvider>
-					<Refine
-						routerProvider={routerProvider}
-						dataProvider={dataProvider(supabaseClient)}
-						authProvider={authProvider}
-						notificationProvider={notificationProvider}
-						resources={[
-							{
-								name: "services",
-								list: "/services",
-								create: "/services/create",
-								edit: "/services/edit/:id",
-								show: "/services/show/:id",
-								meta: {
-									canDelete: true,
-								},
-							},
-							{
-								name: "posts",
-								list: "/posts",
-								create: "/posts/create",
-								edit: "/posts/edit/:id",
-								show: "/posts/show/:id",
-								meta: {
-									canDelete: true,
-								},
-							},
-						]}
-						options={{
-							syncWithLocation: true,
-							warnWhenUnsavedChanges: true,
-						}}
-					>
-						{renderComponent()}
-						<RefineKbar />
-						<UnsavedChangesNotifier />
-					</Refine>
-				</ColorModeContextProvider>
-			</RefineKbarProvider>
-		</>
-	);
+  return (
+    <>
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <Refine
+            routerProvider={routerProvider}
+            dataProvider={dataProvider(supabaseClient)}
+            authProvider={authProvider}
+            notificationProvider={notificationProvider}
+            resources={[
+              {
+                name: "services",
+                list: "/services",
+                create: "/services/create",
+                edit: "/services/edit/:id",
+                show: "/services/show/:id",
+                meta: {
+                  canDelete: true,
+                },
+              },
+              {
+                name: "posts",
+                list: "/posts",
+                create: "/posts/create",
+                edit: "/posts/edit/:id",
+                show: "/posts/show/:id",
+                meta: {
+                  canDelete: true,
+                },
+              },
+            ]}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+            }}
+          >
+            {renderComponent()}
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+          </Refine>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </>
+  );
 }
 
 export default MyApp;
